2c26385fb3bfcf1636716f699d7503cdd4d84707
xiphl <xipherl9@gmail.com>
2021-11-15 23:22:15 +0700

    EOD 15 nov

--- datahub-web-react/src/app/entity/shared/tabs/Dataset/Schema/EditSchemaTable.tsx
+++ datahub-web-react/src/app/entity/shared/tabs/Dataset/Schema/EditSchemaTable.tsx
@@ -1,23 +1,35 @@
 // import { Empty } from 'antd';
 import React from 'react';
 // import { useGetAuthenticatedUser } from '../../../../../useGetAuthenticatedUser';
-import { Table } from 'antd'; // https://ant.design/components/table/#components-table-demo-basic
+import { Table, Divider } from 'antd'; // https://ant.design/components/table/#components-table-demo-basic
+import { useBaseEntity } from '../../../EntityContext';
+import { GetDatasetQuery } from '../../../../../../graphql/dataset.generated';
 
 export const EditSchemaTable = () => {
-    const dataSource = [
-        {
-            key: '1',
-            name: 'Mike',
-            age: 32,
-            address: '10 Downing Street',
-        },
-        {
-            key: '2',
-            name: 'John',
-            age: 42,
-            address: '10 Downing Street',
-        },
-    ];
+    const queryFields = useBaseEntity<GetDatasetQuery>()?.dataset?.schemaMetadata?.fields;
+    const dataSource = queryFields?.map((x, ind) => {
+        return {
+            key: ind,
+            name: x?.fieldPath,
+            datahubType: x?.type,
+            nativeType: x?.nativeDataType,
+        };
+    });
+    console.log(dataSource);
+    // const dataSource = [
+    //     {
+    //         key: '1',
+    //         name: 'Mike',
+    //         age: 32,
+    //         address: '10 Downing Street',
+    //     },
+    //     {
+    //         key: '2',
+    //         name: 'John',
+    //         age: 42,
+    //         address: '10 Downing Street',
+    //     },
+    // ];
     const columns = [
         {
             title: 'Name',
@@ -25,20 +37,21 @@ export const EditSchemaTable = () => {
             key: 'name',
         },
         {
-            title: 'Age',
-            dataIndex: 'age',
-            key: 'age',
+            title: 'Type',
+            dataIndex: 'datahubType',
+            key: 'datahubType',
         },
         {
-            title: 'Address',
-            dataIndex: 'address',
-            key: 'address',
+            title: 'NativeType',
+            dataIndex: 'nativeType',
+            key: 'nativeType',
         },
     ];
     // const currUser = useGetAuthenticatedUser()?.corpUser?.username || '-';
     return (
         <>
             Edit Dataset Schema here:
+            <Divider />
             <Table dataSource={dataSource} columns={columns} />;
         </>
     );

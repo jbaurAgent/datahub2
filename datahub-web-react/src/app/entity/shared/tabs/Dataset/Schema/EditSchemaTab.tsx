872699283bb222ccb0ec0ba24a163cca5690470d
xiphl <xipherl9@gmail.com>
2021-11-17 21:57:58 +0700

    custom graphql query

--- datahub-web-react/src/app/entity/shared/tabs/Dataset/Schema/EditSchemaTab.tsx
+++ datahub-web-react/src/app/entity/shared/tabs/Dataset/Schema/EditSchemaTab.tsx
@@ -1,7 +1,9 @@
 // import { Empty } from 'antd';
 import React from 'react';
+// import { getBrowsePathsResolver } from '../../../../../../graphql-mock/resolver/getBrowsePathsResolver';
+// import { gql, useQuery } from '@apollo/client';
 import { GetDatasetOwnersSpecialQuery, GetDatasetQuery } from '../../../../../../graphql/dataset.generated';
-import { BrowsePath } from '../../../../../../types.generated';
+// import { BrowsePath } from '../../../../../../types.generated';
 import { useGetAuthenticatedUser } from '../../../../../useGetAuthenticatedUser';
 import { useBaseEntity } from '../../../EntityContext';
 import { DeleteSchemaTab } from './DeleteSchemaTab';
@@ -9,14 +11,32 @@ import { DeleteSchemaTab } from './DeleteSchemaTab';
 import EditSchemaTableNew from './EditSchemaTableNew';
 // import EditSchemaTableNew from './EditSchemaTableNew';
 
+// import { EntityType } from '../../../../../../types.generated';
+
 export const EditSchemaTab = () => {
     const queryBase = useBaseEntity<GetDatasetOwnersSpecialQuery>()?.dataset?.ownership?.owners;
     const ownersArray = queryBase?.map((x) => (x?.type === 'DATAOWNER' ? x?.owner?.urn.split(':').slice(-1) : ''));
     const ownersArray2 = ownersArray?.flat() ?? [];
     const currUser = useGetAuthenticatedUser()?.corpUser?.username || '-';
     const queryFields = useBaseEntity<GetDatasetQuery>()?.dataset?.schemaMetadata?.fields;
-    const browse = useBaseEntity<BrowsePath>()?.path; // check for ideas here: /home/admini/newtry/datahub/datahub-web-react/src/app/entity/shared/containers/profile/EntityProfile.tsx
-    console.log(`browsepath is ${browse}`);
+    const baseEntity = useBaseEntity<GetDatasetQuery>();
+    const currUrn = baseEntity && baseEntity.dataset && baseEntity.dataset?.urn;
+    console.log(currUrn);
+    // const queryresult = gql`
+    //     {
+    //         browsePaths(
+    //             input: {
+    //                 urn: "${currUrn}"
+    //                 type: DATASET
+    //             }
+    //         ) {
+    //             path
+    //         }
+    //     }
+    // `;
+    // const { data } = useQuery(queryresult);
+    // console.log(data.browsePaths.length);
+    // data.browsePaths.map((x) => console.log(x.path));
     if (ownersArray2.includes(currUser)) {
         return (
             <>

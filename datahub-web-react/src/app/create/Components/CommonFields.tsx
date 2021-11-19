67731ed8565f65be67720bc17a9117fc40b9169b
xiphl <xipherl9@gmail.com>
2021-11-19 17:04:38 +0800

    adhoc create form

--- datahub-web-react/src/app/create/Components/CommonFields.tsx
+++ datahub-web-react/src/app/create/Components/CommonFields.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Form, Input } from 'antd';
+import { Button, Form, Input, Space } from 'antd';
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 
 export const CommonFields = () => {
     return (
@@ -52,6 +53,60 @@ export const CommonFields = () => {
             >
                 <Input />
             </Form.Item>
+            <Form.List
+                name="browsepaths"
+                rules={[
+                    {
+                        validator: async (_, browsepaths) => {
+                            if (!browsepaths || browsepaths.length < 1) {
+                                return new Error('At least 1 browsepath is needed');
+                            }
+                            if (!browsepaths || browsepaths.length > 3) {
+                                return new Error('no more than 3 browsepath');
+                            }
+                            return true;
+                        },
+                    },
+                ]}
+            >
+                {(fields, { add, remove }, { errors }) => (
+                    <>
+                        <Space style={{ display: 'inline', marginBottom: 8 }} align="baseline">
+                            {fields.map((field) => (
+                                <Form.Item label="Browsing Paths for Dataset" required key={field.key}>
+                                    <Form.Item
+                                        {...field}
+                                        validateTrigger={['onChange', 'onBlur']}
+                                        rules={[
+                                            {
+                                                required: true,
+                                                whitespace: true,
+                                                pattern: new RegExp(/\/[0-9a-zA-Z /]+$/),
+                                                message: 'Please input a path starting with a / ',
+                                            },
+                                        ]}
+                                        noStyle
+                                    >
+                                        <Input placeholder="browsing path" style={{ width: '30%' }} />
+                                    </Form.Item>
+                                    {fields.length > 1 ? (
+                                        <MinusCircleOutlined
+                                            className="dynamic-delete-button"
+                                            onClick={() => remove(field.name)}
+                                        />
+                                    ) : null}
+                                </Form.Item>
+                            ))}
+                            <Form.Item>
+                                <Button type="dashed" onClick={() => add()} icon={<PlusOutlined />}>
+                                    Add more browsing paths
+                                </Button>
+                                <Form.ErrorList errors={errors} />
+                            </Form.Item>
+                        </Space>
+                    </>
+                )}
+            </Form.List>
         </>
     );
 };
